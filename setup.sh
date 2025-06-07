377360d3681a8da883fa284fdff7d3f6e51af972
David Dasenbrook <david.dasenbrook@gmail.com>
2025-06-07 12:50:05 +0200

    add a test deployment and a local volume

--- setup.sh
+++ setup.sh
@@ -14,25 +14,30 @@
 sudo -u postgres psql -c "CREATE USER strapi WITH PASSWORD 'strapi';"
 sudo -u postgres psql -c "CREATE DATABASE strapi OWNER strapi;"
 
+# Set up a data directory for local volumes
+mkdir -p /var/data
+
 # Install and setup k0s
-curl -sSf https://get.k0s.sh | sudo sh
-sudo k0s install controller --single
-sudo k0s start
+curl -sSf https://get.k0s.sh | sh
+k0s install controller --single
+k0s start
 
 # wait for k0s to be ready
-while ! sudo k0s status | grep -q "k0s is running"; do
+while ! k0s status | grep -q "k0s is running"; do
     echo "Waiting for k0s to be ready..."
     sleep 5
 done
 
 # Install NGINX ingress controller
-sudo k0s kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.3/deploy/static/provider/baremetal/deploy.yaml
+k0s kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.3/deploy/static/provider/baremetal/deploy.yaml
 
 # Wait for NGINX ingress controller to be ready
-while ! sudo k0s kubectl get pods -n ingress-nginx | grep -q "1/1"; do
+while ! k0s kubectl get pods -n ingress-nginx | grep -q "1/1"; do
     echo "Waiting for NGINX ingress controller to be ready..."
     sleep 5
 done
 
-sudo k0s kubectl -n ingress-nginx annotate ingressclasses nginx ingressclass.kubernetes.io/is-default-class="true"
+k0s kubectl -n ingress-nginx annotate ingressclasses nginx ingressclass.kubernetes.io/is-default-class="true"
+
+k0s kubectl apply -f workloads/volume/local.yaml
 
